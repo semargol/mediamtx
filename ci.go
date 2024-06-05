f40a5d19acfc5087c41d3910c83e138af54c1177
Oleg Iodynis <oleg.iodynis@gmail.com>
2024-06-06 00:19:34 +0300

    ci from another host

--- ci.go
+++ ci.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
+	if len(os.Args) > 2 {
+		control.RunControl(os.Args[1], "/ci", os.Args[2])
+	} else if len(os.Args) > 1 {
 		control.RunControl(":7002", "/ci", os.Args[1])
 	} else {
 		control.RunControl(":7002", "/ci", "ci.ini") // message broker URL is ws://127.0.0.1:7000/ci
